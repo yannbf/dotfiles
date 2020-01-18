7b76d845b001f605573ec576efa73ecee10609a4
Yann Braga <yannbf@gmail.com>
2020-01-18 13:00:35 +0100

    feat(alias): add extra aliases
    - Open chrome
    - Show/hide desktop icons
    - Colored grep
    - Http methods
    - cd -

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,20 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 alias quit='exit' # It's just easier to type
+alias :q='exit'
+
+alias -- -="cd -"
+alias grep='grep --color=auto'
+
+alias ip='ipconfig getifaddr en0'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+# for easy http requests
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	alias "${method}"="lwp-request -m '${method}'"
+done

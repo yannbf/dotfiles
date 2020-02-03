c56733ece77cf0ccb3c3b72da97f1561576c365b
Yann Braga <yannbf@gmail.com>
2020-02-03 11:20:19 +0100

    chore: add folder check on yarn script

--- yarn/install.sh
+++ yarn/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
 if type "yarn" > /dev/null; then
-  echo "\033[1;33m› Adding permission to .config folder(necessary for git, yarn, etc)..\033[0m"
-  sudo chown -R $USER:$GROUP $HOME/.config
+  if [ -d $HOME/.config ]; then
+    echo "\033[1;33m› Adding permission to .config folder(necessary for git, yarn, etc)..\033[0m"
+    sudo chown -R $USER:$GROUP $HOME/.config
+  else 
+    echo "\033[1;33m› .config folder does not exist, please run ./dotfiles/yarn/install later!\033[0m"
+  fi
 fi

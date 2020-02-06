cad6cd1ee5659b2db923ef330ce8c38216485f92
Yann Braga <yannbf@gmail.com>
2020-02-06 11:14:24 +0100

    chore(git): add go to master alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -24,3 +24,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias ggm='git checkout master && git pull'

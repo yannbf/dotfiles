fcbde47e72dffd976dbfc659631b1306d42fd085
Yann Braga <yannbf@gmail.com>
2020-05-01 13:47:09 +0200

    fix: run brew update script on dot exec

--- homebrew/update.sh
+++ homebrew/update.sh
@@ -4,13 +4,47 @@
 #
 # This updates/installs all aplications from the Brewfile.
 
-echo -e "\033[1;33m› Installing all applications from the Brewfile..\033[0m"
+ask() {
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
+
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+
+echo "\033[1;33m› Installing or updating all applications from the Brewfile..\033[0m"
 brew bundle --file=$HOME/.dotfiles/homebrew/Brewfile
 
-echo -e "\033[1;33m› Install/update your app store applications from your HomebrewAppStore file? This process might take a long time."
-read REPLY\?"You can install later by running update-brew (y/n) "
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo -e "\033[1;33m› Installing all applications from the Brewfile..\033[0m"
+if ask "\033[1;33m› Install/update your app store applications from your HomebrewAppStore file? This process might take a long time.\nYou can install later by running update-brew"; then
+  echo "\033[1;33m› Installing all app store applications from the Brewfile..\033[0m"
   brew bundle --file=$HOME/.dotfiles/homebrew/BrewfileAppStore
 fi

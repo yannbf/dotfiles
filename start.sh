cb98eb155bf5eff9d9cbced82413278c4cc8859b
Guilherme Waess <guilherme.waess@gmail.com>
2020-05-02 15:22:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles.."
 
-    git clone --depth=1 https://github.com/yannbf/dotfiles.git "$HOME/.dotfiles"
+    git clone --depth=1 https://github.com/guilhermewaess/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
     script/bootstrap
 else

bd6a0bf0a03c8fb6148132e1929b4ad1e2dc49a6
Yann Braga <yannbf@gmail.com>
2021-01-05 09:17:26 +0100

    alias: add storybook and cordova aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,8 +7,11 @@ alias :q='exit'
 alias yst='yarn storybook'
 alias ys='yarn start'
 alias yb='yarn build'
+alias test='npx jest --watch'
+alias dk='docker-compose up'
+alias dkbuild='docker-compose build'
 
-alias -- -="cd -"
+alias --="cd -"
 alias grep='grep --color=auto'
 
 alias ip='ipconfig getifaddr en0 && ipconfig getifaddr en0 | pbcopy'
@@ -23,3 +26,13 @@ alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
 	alias "${method}"="lwp-request -m '${method}'"
 done
+
+# storybook monorepo specific
+alias streact='yarn --cwd examples/official-storybook storybook'
+alias staurelia='yarn --cwd examples/aurelia-kitchen-sink storybook'
+alias stangular='yarn --cwd examples/angular-cli storybook'
+alias stvue='yarn --cwd examples/vue-kitchen-sink storybook'
+alias stsvelte='yarn --cwd examples/svelte-kitchen-sink storybook'
+
+# hybrid-core specific
+alias iphone='cd dist/app && cordova platform add ios@5.0.1 && cordova platform remove ios && cordova platform add ios@5.0.1 && cordova run ios'
